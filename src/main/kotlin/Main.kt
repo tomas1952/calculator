2a1291147fe9da421b5adf595e488140ac15b019
elaq0625 <kevin@elaq0625ui-MacBookPro.local>
2023-12-05 15:54:25 +0900

    feat: add CalculatorIntegerTest

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,23 +3,7 @@ import level2.Operator.*
 
 fun main(args: Array<String>) {
     val calculator = Calculator()
+    val result = calculator.operating(plus, 10, 3)
 
-    val a0 = 10
-    val b0 = 3
-
-    println("정수형 연산")
-    println("$a0 + $b0 = ${13} -> ${calculator.operating(plus, a0, b0)}")
-    println("$a0 - $b0 = ${7} -> ${calculator.operating(minus, a0, b0)}")
-    println("$a0 * $b0 = ${30} -> ${calculator.operating(multiple, a0, b0)}")
-    println("$a0 / $b0 = ${3} -> ${calculator.operating(divide, a0, b0)}")
-    println("$a0 % $b0 = ${1} -> ${calculator.operating(mod, a0, b0)}")
-
-    val a1 = 10.0
-    val b1 = 3.0
-
-    println("$a1 + $b1 = ${13} -> ${calculator.operating(plus, a1, b1)}")
-    println("$a1 - $b1 = ${7} -> ${calculator.operating(minus, a1, b1)}")
-    println("$a1 * $b1 = ${30} -> ${calculator.operating(multiple, a1, b1)}")
-    println("$a1 / $b1 = ${3} -> ${calculator.operating(divide, a1, b1)}")
-    println("$a1 % $b1 = ${1} -> ${calculator.operating(mod, a1, b1)}")
+    println(result)
 }
---
+++ src/test/kotlin/level2/CalculatorIntegerTest.kt
@@ -0,0 +1,71 @@
+package level2
+import io.kotest.assertions.throwables.shouldThrowAny
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import level2.Operator.*
+
+class CalculatorIntegerTest : BehaviorSpec({
+    val calculator = Calculator()
+
+    Given("10과 3이 주어졌을때") {
+        val a = 10
+        val b = 3
+
+        When("계산기로 더하면") {
+            val result = calculator.operating(plus, a, b)
+            Then("13이 나와야 한다.") {
+                result shouldBe 13
+            }
+        }
+
+        When("계산기로 빼면") {
+            val result = calculator.operating(minus, a, b)
+            Then("7이 나와야 한다.") {
+                result shouldBe 7
+            }
+        }
+
+        When("계산기로 곱하면") {
+            val result = calculator.operating(multiple, a, b)
+            Then("30이 나와야 한다.") {
+                result shouldBe 30
+            }
+        }
+
+        When("계산기로 나누면") {
+            val result = calculator.operating(divide, a, b)
+            Then("3이 나와야 한다.") {
+                result shouldBe 3
+            }
+        }
+
+        When("계산기로 나머지 연산을 하면") {
+            val result = calculator.operating(mod, a, b)
+            Then("1이 나와야 한다.") {
+                result shouldBe 1
+            }
+        }
+    }
+
+
+    Given("10과 0이 주어졌을때") {
+        val a = 10
+        val b = 0
+
+        When("계산기로 나누면") {
+            Then("에러가 발생한다.") {
+                shouldThrowAny {
+                    calculator.operating(divide, a, b)
+                }
+            }
+        }
+
+        When("계산기로 나머지 연산을 하면") {
+            Then("에러가 발생한다.") {
+                shouldThrowAny {
+                    calculator.operating(divide, a, b)
+                }
+            }
+        }
+    }
+})
