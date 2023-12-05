0bb2bc65a5c082a1dd010630c6c2bf3d4d3c8a01
elaq0625 <kevin@elaq0625ui-MacBookPro.local>
2023-12-05 16:50:31 +0900

    feat: float,double, long test code

---
+++ src/test/kotlin/level2/CalculatorDoubleTest.kt
@@ -0,0 +1,55 @@
+package level2
+import exception.CustomDividedZeroException
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.comparables.shouldBeGreaterThan
+import io.kotest.matchers.comparables.shouldBeLessThan
+import io.kotest.matchers.floats.shouldBeLessThan
+import io.kotest.matchers.shouldBe
+import level2.Operator.*
+
+class CalculatorDoubleTest : BehaviorSpec({
+    val calculator = Calculator()
+
+    Given("10과 3이 주어졌을때") {
+        val a = 10.0
+        val b = 0.3
+
+        When("계산기로 더하면") {
+            val result = calculator.operating(plus, a, b)
+            Then("10.3이 나와야 한다.") {
+                result shouldBe 10.3
+            }
+        }
+
+        When("계산기로 빼면") {
+            val result = calculator.operating(minus, a, b)
+            Then("9.7이 나와야 한다.") {
+                result shouldBe 9.7
+            }
+        }
+
+        When("계산기로 곱하면") {
+            val result = calculator.operating(multiple, a, b)
+            Then("3이 나와야 한다.") {
+                result shouldBe 3.0
+            }
+        }
+
+        When("계산기로 나누면") {
+            val result = calculator.operating(divide, a, b)
+            Then("33.33332 ~ 33.33334 값이 나와야 한다.") {
+                result shouldBeGreaterThan 33.33332
+                result shouldBeLessThan 33.33334
+            }
+        }
+
+        When("계산기로 나머지 연산을 하면") {
+            val result = calculator.operating(mod, a, b)
+            Then("0.1이 나와야 한다.") {
+                result shouldBeGreaterThan  0.0999999
+                result shouldBeLessThan  0.1000001
+            }
+        }
+    }
+})
---
+++ src/test/kotlin/level2/CalculatorLongTest.kt
@@ -0,0 +1,51 @@
+package level2
+import exception.CustomDividedZeroException
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.assertions.throwables.shouldThrowAny
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import level2.Operator.*
+
+class CalculatorLongTest : BehaviorSpec({
+    val calculator = Calculator()
+
+    Given("10과 3이 주어졌을때") {
+        val a = 10L
+        val b = 3L
+
+        When("계산기로 더하면") {
+            val result = calculator.operating(plus, a, b)
+            Then("13이 나와야 한다.") {
+                result shouldBe 13L
+            }
+        }
+
+        When("계산기로 빼면") {
+            val result = calculator.operating(minus, a, b)
+            Then("7이 나와야 한다.") {
+                result shouldBe 7L
+            }
+        }
+
+        When("계산기로 곱하면") {
+            val result = calculator.operating(multiple, a, b)
+            Then("30이 나와야 한다.") {
+                result shouldBe 30L
+            }
+        }
+
+        When("계산기로 나누면") {
+            val result = calculator.operating(divide, a, b)
+            Then("3이 나와야 한다.") {
+                result shouldBe 3L
+            }
+        }
+
+        When("계산기로 나머지 연산을 하면") {
+            val result = calculator.operating(mod, a, b)
+            Then("1이 나와야 한다.") {
+                result shouldBe 1L
+            }
+        }
+    }
+})
