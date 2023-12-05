151882dc2be89b0e28330d256b53fbdcb6d941d0
elaq0625 <kevin@elaq0625ui-MacBookPro.local>
2023-12-05 17:30:28 +0900

    fix: add ModOperation DivideZeror Exception case

--- src/main/kotlin/level4/operation/ModOperation.kt
+++ src/main/kotlin/level4/operation/ModOperation.kt
@@ -1,29 +1,101 @@
 package level4.operation
 
+import exception.CustomDividedZeroException
+
 class ModOperation : AbstractOperation {
     // Int
-    override fun operate(a: Int, b: Int): Int = a.mod(b)
-    override fun operate(a: Int, b: Long): Long = a.mod(b)
-    override fun operate(a: Int, b: Float): Float  = a.toFloat().mod(b)
-    override fun operate(a: Int, b: Double): Double  = a.toDouble().mod(b)
+    override fun operate(a: Int, b: Int): Int = if (b == 0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.mod(b)
+    }
+
+    override fun operate(a: Int, b: Long): Long = if (b == 0L) {
+        throw CustomDividedZeroException()
+    } else {
+        a.mod(b)
+    }
+    override fun operate(a: Int, b: Float): Float = if (b == 0f) {
+        throw CustomDividedZeroException()
+    } else {
+        a.toFloat().mod(b)
+    }
+
+    override fun operate(a: Int, b: Double): Double = if (b == 0.0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.toDouble().mod(b)
+    }
     
     // Long
-    override fun operate(a: Long, b: Int): Long  = a.mod(b.toLong())
-    override fun operate(a: Long, b: Long): Long  = a.mod(b)
-    override fun operate(a: Long, b: Float): Float  = a.toFloat().mod(b)
-    override fun operate(a: Long, b: Double): Double  = a.toDouble().mod(b)
-    
+    override fun operate(a: Long, b: Int): Long = if (b == 0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.mod(b.toLong())
+    }
+
+    override fun operate(a: Long, b: Long): Long = if (b == 0L) {
+        throw CustomDividedZeroException()
+    } else {
+        a.mod(b)
+    }
+    override fun operate(a: Long, b: Float): Float = if (b == 0f) {
+        throw CustomDividedZeroException()
+    } else {
+        a.toFloat().mod(b)
+    }
+    override fun operate(a: Long, b: Double): Double = if (b == 0.0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.toDouble().mod(b)
+    }
+
     // Float
-    override fun operate(a: Float, b: Int): Float  = a.mod(b.toFloat())
-    override fun operate(a: Float, b: Long): Float  = a.mod(b.toFloat())
-    override fun operate(a: Float, b: Float): Float  = a.mod(b)
-    override fun operate(a: Float, b: Double): Double  = a.mod(b)
+    override fun operate(a: Float, b: Int): Float = if (b == 0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.mod(b.toFloat())
+    }
+
+    override fun operate(a: Float, b: Long): Float = if (b == 0L) {
+        throw CustomDividedZeroException()
+    } else {
+        a.mod(b.toFloat())
+    }
+
+    override fun operate(a: Float, b: Float): Float = if (b == 0f) {
+        throw CustomDividedZeroException()
+    } else {
+        a.mod(b)
+    }
+
+    override fun operate(a: Float, b: Double): Double = if (b == 0.0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.toDouble().mod(b)
+    }
     
     // Double
 
-    override fun operate(a: Double, b: Int): Double  = a.mod(b.toDouble())
-    override fun operate(a: Double, b: Long): Double  = a.mod(b.toDouble())
-    override fun operate(a: Double, b: Float): Double  = a.mod(b)
-    override fun operate(a: Double, b: Double): Double  = a.mod(b)
+    override fun operate(a: Double, b: Int): Double = if (b == 0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.mod(b.toDouble())
+    }
+    override fun operate(a: Double, b: Long): Double = if (b == 0L) {
+        throw CustomDividedZeroException()
+    } else {
+        a.mod(b.toDouble())
+    }
+    override fun operate(a: Double, b: Float): Double  = if (b == 0f) {
+        throw CustomDividedZeroException()
+    } else {
+        a.mod(b)
+    }
+    override fun operate(a: Double, b: Double): Double  = if (b == 0.0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.mod(b)
+    }
 
 }
