e7be117aaad25c4afaf114b8db24f31c8ad7f371
elaq0625 <kevin@elaq0625ui-MacBookPro.local>
2023-12-05 17:19:53 +0900

    fix: add DivideOperation divided zero error case

--- src/main/kotlin/level4/operation/DivideOperation.kt
+++ src/main/kotlin/level4/operation/DivideOperation.kt
@@ -1,29 +1,96 @@
 package level4.operation
 
+import exception.CustomDividedZeroException
+
 class DivideOperation : AbstractOperation {
     // Int
-    override fun operate(a: Int, b: Int): Int = a.div(b)
-    override fun operate(a: Int, b: Long): Long = a.div(b)
-    override fun operate(a: Int, b: Float): Float  = a.div(b)
-    override fun operate(a: Int, b: Double): Double  = a.div(b)
+    override fun operate(a: Int, b: Int): Int = if (b == 0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
+    override fun operate(a: Int, b: Long): Long = if (b == 0L) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
+    override fun operate(a: Int, b: Float): Float  = if (b == 0f) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
+
+    override fun operate(a: Int, b: Double): Double  = if (b == 0.0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
     
     // Long
-    override fun operate(a: Long, b: Int): Long  = a.div(b)
-    override fun operate(a: Long, b: Long): Long  = a.div(b)
-    override fun operate(a: Long, b: Float): Float  = a.div(b)
-    override fun operate(a: Long, b: Double): Double  = a.div(b)
+    override fun operate(a: Long, b: Int): Long  = if (b == 0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
+    override fun operate(a: Long, b: Long): Long  = if (b == 0L) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
+    override fun operate(a: Long, b: Float): Float  = if (b == 0f) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
+    override fun operate(a: Long, b: Double): Double  = if (b == 0.0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
     
     // Float
-    override fun operate(a: Float, b: Int): Float  = a.div(b)
-    override fun operate(a: Float, b: Long): Float  = a.div(b)
-    override fun operate(a: Float, b: Float): Float  = a.div(b)
-    override fun operate(a: Float, b: Double): Double  = a.div(b)
+    override fun operate(a: Float, b: Int): Float  = if (b == 0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
+    override fun operate(a: Float, b: Long): Float  = if (b == 0L) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
+    override fun operate(a: Float, b: Float): Float  = if (b == 0f) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
+    override fun operate(a: Float, b: Double): Double  = if (b == 0.0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
     
     // Double
 
-    override fun operate(a: Double, b: Int): Double  = a.div(b)
-    override fun operate(a: Double, b: Long): Double  = a.div(b)
-    override fun operate(a: Double, b: Float): Double  = a.div(b)
-    override fun operate(a: Double, b: Double): Double  = a.div(b)
+    override fun operate(a: Double, b: Int): Double  = if (b == 0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
+    override fun operate(a: Double, b: Long): Double  = if (b == 0L) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
+    override fun operate(a: Double, b: Float): Double  = if (b == 0f) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
+    override fun operate(a: Double, b: Double): Double  = if (b == 0.0) {
+        throw CustomDividedZeroException()
+    } else {
+        a.div(b)
+    }
 
 }
